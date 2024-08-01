66564f51d031be36615975bac3881839bb637e46
Sannonthachai <sannonthachai.t@globish.co.th>
2024-08-01 16:10:31 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,123 @@
+<script setup></script>
+
+<template>
+  <div class="container mx-auto">
+    <div class="grid grid-cols-6 gap-4">
+      <div class="col-span-2">
+        <div class="rounded-full border-2 w-36 h-36"></div>
+      </div>
+      <div class="col-span-4">
+        <div class="text-2xl mt-2 text-teal-800">
+          Sannonthachai Tunphainich (Chai)
+        </div>
+        <div class="text-lg mt-2">092-296-2899, sannonthachai@gmail.com</div>
+        <div class="mt-2 text-xs text-gray-600">
+          — Experience in development of applications in multiple platforms
+          (Full Stack) 5 years. Highly proficient in programming languages such
+          as Javascript, Typescript, Golang.
+        </div>
+      </div>
+    </div>
+
+    <div class="grid grid-cols-6 gap-4 mt-12">
+      <div class="col-span-2 text-xl">Work Experience</div>
+      <div class="col-span-4 mt-4 border-t-2 w-full"></div>
+
+      <div class="col-span-2 text-xs ml-4 mt-2">Sep 2021 - Present</div>
+      <div class="col-span-4 text-xs mt-2">
+        <div>
+          Senior Full Stack Developer - Globish Academia (Thailand) Co., Ltd.
+        </div>
+        <div class="text-gray-600">
+          <li class="mt-1">Migration a monolith project into microservices.</li>
+          <li class="mt-1">Migration a monolith project into microservices.</li>
+          <li class="mt-1">Migration a monolith project into microservices.</li>
+        </div>
+      </div>
+
+      <div class="col-span-2 text-xs ml-4 mt-2">Sep 2021 - Present</div>
+      <div class="col-span-4 text-xs mt-2">
+        <div>
+          Senior Full Stack Developer - Globish Academia (Thailand) Co., Ltd.
+        </div>
+        <div class="text-gray-600">
+          <li class="mt-1">Migration a monolith project into microservices.</li>
+          <li class="mt-1">Migration a monolith project into microservices.</li>
+          <li class="mt-1">Migration a monolith project into microservices.</li>
+        </div>
+      </div>
+    </div>
+
+    <div class="grid grid-cols-6 gap-4 mt-6">
+      <div class="col-span-2 text-xl">Software Skills</div>
+      <div class="col-span-4 mt-4 border-t-2 w-full"></div>
+
+      <div class="col-span-6 text-xs">
+        <div class="ml-4 mt-3">
+          <div>Back-end</div>
+          <div class="text-gray-600 mt-2">
+            Express | NestJS | Jest | Echo ( Golang ) | Cobra ( Golang Monorepo
+            ) | SQL & NO-SQL Database | Redis | Web Socket | SSE | Bull MQ | Nx
+            ( Monorepo )
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="grid grid-cols-6 gap-4 mt-12">
+      <div class="col-span-2 text-xl">Education</div>
+      <div class="col-span-4 mt-4 border-t-2 w-full"></div>
+
+      <div class="col-span-2 text-xs ml-4 mt-2">2014 - 2018</div>
+      <div class="col-span-4 text-xs mt-2">
+        <div>
+          Bachelor of Science - Major in Statistical Science King Mongkut’s
+          University of Technology Thonburi
+        </div>
+      </div>
+
+      <div class="col-span-2 text-xs ml-4 mt-2">2008 - 2014</div>
+      <div class="col-span-4 text-xs mt-2">
+        <div>
+          Bachelor of Science - Major in Statistical Science King Mongkut’s
+          University of Technology Thonburi
+        </div>
+      </div>
+    </div>
+
+    <div class="grid grid-cols-6 gap-4 mt-6">
+      <div class="col-span-2 text-xl">Github</div>
+      <div class="col-span-4 mt-4 border-t-2 w-full"></div>
+
+      <div class="col-span-6 text-xs">
+        <div class="ml-4 mt-3">
+          SSE with golang - https://github.com/sannonthachai/go-poc-sse
+        </div>
+        <div class="ml-4 mt-3">
+          SSE with golang - https://github.com/sannonthachai/go-poc-sse
+        </div>
+        <div class="ml-4 mt-3">
+          SSE with golang - https://github.com/sannonthachai/go-poc-sse
+        </div>
+        <div class="ml-4 mt-3">
+          SSE with golang - https://github.com/sannonthachai/go-poc-sse
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
